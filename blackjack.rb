d889def14213da767804eaa87e84bd44faf32268
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-01-14 16:46:15 +0200

    Reworking based on Barndon's comments

--- blackjack.rb
+++ blackjack.rb
@@ -3,99 +3,102 @@ def say(msg)
   puts msg
 end
 
-def show_hand(player_hand)
+SUITS = ["Hearts","Clubs","Spades","Diamonds"]
+CARDS = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
+
+deck = SUITS.product(CARDS) 
+deck.shuffle!
+
+def show_hand(hand)
   puts 
-  player_hand.each do |card|
-    puts "=> #{card}"
+  hand.each do |card|
+    puts "=> #{card[1]} of #{card[0]}"
   end
 end
 
-def hit(deck_cards, deck_suits, deck_values, player_hand, player_values, player_suits, player_cards)
-  card = deck_cards.sample
-  suit = deck_suits[deck_cards.index(card)]
-  if card == "Ace"
-    value = 1  
-  elsif (card == "Jack" || card == "Queen" || card == "King")
-    value = 10
-  else 
-    value = card 
+def hit(hand, deck)
+  a = deck.pop
+  hand << a
+end
+
+def score(hand)
+  total = 0
+  values = []
+  value = 0
+  hand.each do |card|
+    if card[1] == "Ace"
+      total += 1 
+    elsif (card [1] == "Jack" || card [1] == "Queen" || card [1] == "King")
+      total+=10
+    else 
+      total+=card[1].to_i
+    end
   end
-  player_values.push(value)
-  player_cards.push(card)
-  player_suits.push(suit) 
-  player_hand.push("#{card} of #{suit}")
-  deck_cards.delete_at(deck_cards.index(card))
-  deck_suits.delete_at(deck_suits.index(suit))
-  deck_values.delete_at(deck_values.index(value))
+  total += 10 if (total<12 && values.include?(1))
+  total
 end
 
-def score(player_values)
-   total = 0
-   player_values.each do |value|
-     total+=value  
-   end
-   total += 10 if (total<12 && player_values.include?(1))
-   total
-end
-
-def mean(deck_values)
+def mean(deck)
   sum = 0
-  deck_values.each do |card|
-    sum+=card
+  deck.each do |card|
+    if card[1] == "Ace"
+      sum += 1 
+    elsif (card [1] == "Jack" || card [1] == "Queen" || card [1] == "King")
+      sum+=10
+    else 
+      sum+=card[1].to_i
+    end
   end
-  mean = sum.to_f/deck_values.length
+  mean = sum.to_f/deck.length
+  mean.round(2)
+end
+
+def dealer_first_card(hand)
+  say ("One of the dealer's cards is")
+  say ("=> #{hand[0][1]} of #{hand[0][0]}")
 end
 
 begin
   system 'clear'
-  
-  deck_cards = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,"Jack","Jack","Jack","Jack","Queen","Queen","Queen","Queen","King","King","King","King","Ace","Ace","Ace","Ace"]
-  deck_suits = ["hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades","hearts","clubs","diamonds","spades"]
-  deck_values = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,1,1,1,1]
+
+  your_hand = []
+  dealers_hand = []
 
   compare = false
   bust = false
   win = false
   stay = false
   
-  you_hand = Array.new
-  you_values = Array.new
-  you_suits = Array.new
-  you_cards = Array.new
-  dealer_hand = Array.new
-  dealer_values = Array.new
-  dealer_suits = Array.new
-  dealer_cards = Array.new
-  
-  hit(deck_cards, deck_suits, deck_values, you_hand, you_values, you_suits, you_cards)
-  hit(deck_cards, deck_suits, deck_values, you_hand, you_values, you_suits, you_cards)
-  hit(deck_cards, deck_suits, deck_values, dealer_hand, dealer_values, dealer_suits, dealer_cards)
-  hit(deck_cards, deck_suits, deck_values, dealer_hand, dealer_values, dealer_suits, dealer_cards)
-  
+  hit(your_hand, deck)
+  hit(your_hand, deck)
   puts "You were initially dealt these cards:"
-  show_hand(you_hand)
-  say("Your score is #{score(you_values)}.")
+  show_hand(your_hand)
+  say("Your score is #{score(your_hand)}.")
+  hit(dealers_hand, deck)
+  hit(dealers_hand, deck)
   
-  begin
-    if score(you_values) > 21
+  begin 
+    if score(your_hand) > 21
       bust = true
-    elsif score(you_values) == 21
+    elsif score(your_hand) == 21
       win = true
     else 
       begin 
+        dealer_first_card(dealers_hand)
+        say("The mean face value of the remaining cards is #{mean(deck)}")
         say("Do you want to hit or stay? (H/S)")
         decision = gets.chomp.downcase
         if decision ==  'h'
           system 'clear'
           puts "You decided to hit another card. This is your new hand:"
-          hit(deck_cards, deck_suits, deck_values, you_hand, you_values, you_suits, you_cards)
-          show_hand(you_hand)
-          say("The score is #{score(you_values)}") 
+          hit(your_hand, deck)
+          show_hand(your_hand)
+          say("Your score is #{score(your_hand)}") 
         elsif decision == 's'
           system 'clear'
           puts "You decided to stay with the following hand:"
-          show_hand(you_hand)
-          say("Your score is #{score(you_values)}")
+          show_hand(your_hand)
+          say("Your score is #{score(your_hand)}")
           say("It is now the dealer's turn.") 
           stay = true
         end
@@ -108,19 +111,19 @@ begin
 
   if stay
     begin    
-      if score(dealer_values) < 17
-        hit(deck_cards, deck_suits, deck_values, dealer_hand, dealer_values, dealer_suits, dealer_cards)
-      elsif score(dealer_values) > 21
+      if score(dealers_hand) < 17
+        hit(dealers_hand, deck)
+      elsif score(dealers_hand) > 21
         say("The dealer busted.")
         win = true
-      elsif score(dealer_values) == 21
+      elsif score(dealers_hand) == 21
         say("The dealer hit a blackjack with the following hand:")
-        show_hand(dealer_hand)
+        show_hand(dealers_hand)
         bust = true
-      elsif score(dealer_values) + mean(deck_values) > 21
+      elsif (score(dealers_hand) + mean(deck).round(2)) > 21
         compare = true
       else
-        hit(deck_cards, deck_suits, deck_values, dealer_hand, dealer_values, dealer_suits, dealer_cards)  
+        hit(dealers_hand, deck) 
       end
     end until bust || win || compare
   end
@@ -128,22 +131,22 @@ begin
   if compare
     say("The dealer decided to stay too and you are now going to compare hands.")
     say("Dealer's hand is:")
-    show_hand(dealer_hand) 
-    say("Dealer's score is #{score(dealer_values)}.")
+    show_hand(dealers_hand) 
+    say("Dealer's score is #{score(dealers_hand)}.")
   end 
 
-  if bust || (compare && score(you_values)-score(dealer_values)<0)
+  if bust || (compare && score(your_hand)-score(dealers_hand)<0)
     say("Dealer wins.") 
   end
 
-  if win || (compare && score(you_values)-score(dealer_values)>0)
+  if win || (compare && score(your_hand)-score(dealers_hand)>0)
     say("You won.") 
   end
 
-  if compare && score(you_values)-score(dealer_values)==0
+  if compare && score(your_hand)-score(dealers_hand)==0
     say("There is a standoff.")
   end
 
   say("Do you want to play again? (Y/N)")
   play_again = gets.chomp.downcase
-end while play_again == "y"  
+end while play_again == "y" 
